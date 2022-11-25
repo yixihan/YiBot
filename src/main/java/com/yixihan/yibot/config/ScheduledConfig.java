4a80e6712c1a39efda3f372814577975632ccaf6
zengsitong <zengsitong@newhope.cn>
2022-11-25 20:24:02 +0800

    新增定时任务配置

---
+++ src/main/java/com/yixihan/yibot/config/ScheduledConfig.java
@@ -0,0 +1,24 @@
+package com.yixihan.yibot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+/**
+ * 定时任务配置类
+ *
+ * @author yixihan
+ * @date 2022/11/25 20:19
+ */
+@Configuration
+public class ScheduledConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduling = new ThreadPoolTaskScheduler();
+        scheduling.setPoolSize(10);
+        scheduling.initialize();
+        return scheduling;
+    }
+}
